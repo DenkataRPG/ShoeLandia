0be0359c2ffbd03091e09bca48689823c6a091b8
DenkataRPG <ikhsandenis07@gmail.com>
2025-02-16 19:22:52 +0200

    Added All Page for Shoe Entity.

---
+++ src/ShoeLandia/Controllers/ShoeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoeLandia.Controllers
+{
+    public class ShoeController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
