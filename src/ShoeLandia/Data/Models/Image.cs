d5f26b48093b264e237740d8fd6081a7d8a97ab0
DenkataRPG <ikhsandenis07@gmail.com>
2025-02-16 18:36:44 +0200

    Added tables to the database.

---
+++ src/ShoeLandia/Data/Models/Image.cs
@@ -0,0 +1,18 @@
+﻿namespace ShoeLandia.Data.Models
+{
+    public class Image 
+    {
+        public Image()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+        public string Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public string ItemId { get; set; }
+        public virtual Item Item { get; set; }
+
+        public string RemoteImageUrl { get; set; }
+        public  string AddedByUserId { get; set; }
+        public  ApplicationUser AddedByUser { get; set; }
+    }
+}
---
+++ src/ShoeLandia/Data/Models/Item.cs
@@ -0,0 +1,27 @@
+﻿namespace ShoeLandia.Data.Models
+{
+    public class Item 
+
+    {
+        public Item()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Images = new HashSet<Image>();
+        }
+
+        public string Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string Size { get; set; }
+        public string Color { get; set; }
+        public virtual Category Category { get; set; }
+        public int CategoryId { get; set; }
+        public string Type { get; set; }
+        public virtual Cart Cart { get; set; }
+        public string CartId { get; set; }
+        public virtual ICollection<Image> Images { get; set; }
+
+    }
+}
