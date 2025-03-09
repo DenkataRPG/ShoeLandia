4fc9b697df07efa4be9707f977fe0bf7b8d746af
DenkataRPG <ikhsandenis07@gmail.com>
2025-03-09 20:49:25 +0200

    Added Service and Controller for Items. Started implementing All Action in ItemController. Updated Item's property.

---
+++ src/ShoeLandia/Services/Interfaces/IItemService.cs
@@ -0,0 +1,9 @@
+﻿using ShoeLandia.ViewModels.Item;
+
+namespace ShoeLandia.Services.Interfaces
+{
+    public interface IItemService
+    {
+        public IEnumerable<SingleItemViewModel> All();
+    }
+}
---
+++ src/ShoeLandia/Services/ItemService.cs
@@ -0,0 +1,40 @@
+﻿using ShoeLandia.Data;
+using ShoeLandia.Services.Interfaces;
+using ShoeLandia.ViewModels.Item;
+
+namespace ShoeLandia.Services
+{
+    public class ItemService : IItemService
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public ItemService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IEnumerable<SingleItemViewModel> All()
+        {
+            var items = this.dbContext
+                .Items
+                .Where(item => item.IsDeleted == false)
+                .Select(item => new SingleItemViewModel()
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    Description = item.Description,
+                    Price = item.Price,
+                    Size = item.Size,
+                    Colors = item.Colors,
+                    CategoryName = item.Category.Name,
+                    CategoryId = item.Category.Id,
+                    Type = item.Type,
+                    Cart = item.Cart,
+                    CartId = item.CartId,
+                    Images = item.Images,   
+
+                })
+                .ToList();
+            return items;
+        }
+    }
+}
