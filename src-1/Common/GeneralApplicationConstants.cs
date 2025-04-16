652f936bae307ab44206adfea6c7ffebbb62fc51
DenkataRPG <ikhsandenis07@gmail.com>
2025-04-17 00:00:04 +0300

    Deleted unnessery files.

--- src-1/Common/GeneralApplicationConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ShoeLandia.Common
-{
-    public static class GeneralApplicationConstants
-    {
-        public const int DefaultPage = 1;
-        public const int EntitiesPerPage = 3;
-    }
-}
